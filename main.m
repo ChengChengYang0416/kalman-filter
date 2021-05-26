295ae5b12ea537798f2cc49513c624b9c9c1297a
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-05-26 11:58:58 +0800

    add pd controller to track the trajectory

--- main.m
+++ main.m
@@ -16,14 +16,21 @@ model.states = zeros(2, length(model.t));
 traj = trajectory;
 tra = zeros(2, length(model.t));
 
+% controller initilaization
+ctrl = controller;
+
 for i = 2:length(model.t)
     t_now = model.t(i);
     
     % 1-D trajectory
     tra(:, i) = traj.traj_generate(t_now);
     
+    % error
+    e = model.states(1, i-1) - tra(1, i-1);
+    e_dot = model.states(2, i-1) - tra(2, i-1);
+    
     % control input
-    u = 1;
+    u = ctrl.pd_controller(e, e_dot);
     
     % dynamics
     X0 = model.states(:, i - 1);
@@ -37,13 +44,10 @@ end
 figure
 subplot(211)
 plot(model.t, model.states(1, :))
-
-subplot(212)
-plot(model.t, model.states(2, :))
-
-figure
-subplot(211)
+hold on
 plot(model.t, tra(1, :))
 
 subplot(212)
+plot(model.t, model.states(2, :))
+hold on
 plot(model.t, tra(2, :))
