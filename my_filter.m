e74c066360b9d6b2b4338297f4a358b0c7c4acd0
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-05-26 16:25:12 +0800

    implement extended kalman filter alogrithm

--- my_filter.m
+++ my_filter.m
@@ -1,8 +1,35 @@
-classdef my_filter
+classdef my_filter < handle
+    properties
+        % for first order low pass filter
+        alpha = 0.2
+        
+        % for extended kalman filter
+        A;
+        C;
+        P = [1, 0, 0;
+             0, 1, 0;
+             0, 0, 1];
+        Q = [2, 0, 0;
+             0, 2, 0;
+             0, 0, 2];
+        R = 1;
+    end
     methods
         function filtered = first_order_lpf(obj, new, old)
-            alpha = 0.2;
-            filtered = alpha*new + (1-alpha)*old;
+            filtered = obj.alpha*new + (1-obj.alpha)*old;
+        end
+        
+        function filtered = extended_kalman_filter(obj, dt, x_last, v_last, u, x_m)
+            obj.A = [1, dt,  0;
+                     0,  1, dt;
+                     0,  0,  0];
+            obj.C = [1, 0, 0];
+            x_predict = [x_last + v_last*dt;
+                         v_last + u*dt;
+                         u];
+            P_predict = obj.A*obj.P*obj.A'+obj.Q;
+            K = P_predict*obj.C'*(inv(obj.C*P_predict*obj.C'+obj.R));
+            filtered = x_predict + K*(x_m - obj.C*x_predict);
         end
     end
 end
